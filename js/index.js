fb3fa4d4fe78ea6db4f89cccc4c8ea688373ab58
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-04-25 16:01:47 -0400

    Overview: The 'progress' of the progress-bar is dependent on three main things namely; the document height, the viewport's height and the viewport's offset to the top of the document

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@
     1a. Listen for click on accordion-header and slide corresponding text down / up
     1b. When panel is visible, give user indication with the caret symbol by adding a class
 
+2. PROGRESSBAR
+    2a. 
 
 
 */
@@ -22,8 +24,8 @@
                 let $body = (opts.accordion.body);
 
                 //1a. 
-                var artcl = document.querySelectorAll($body);           // body
-                var artclHead = document.querySelectorAll($heading);   // heading
+                const artcl = document.querySelectorAll($body);           // body
+                const artclHead = document.querySelectorAll($heading);   // heading
 
                 for (let i = 0; i < artclHead.length; i++) {    // cycle through accordion headers
                     artclHead[i].addEventListener('click', function() {
@@ -49,6 +51,24 @@
                     
                 }
 
+            },
+            
+            progressBar : function () {
+                
+                const progress = (opts.progressbar.progress);            
+                const $progress = document.querySelector(progress);
+                              
+                const windowH = window.innerHeight;   // window Height
+                const documentH = document.documentElement.scrollHeight;  // document Height
+                const amtScrolled = window.scrollY    // amtScrolled
+                const ttlAvailable = documentH - windowH  // How much CAN be scrolled
+                const percent = amtScrolled / ttlAvailable  // What percentage of the scrollable is scrolled : 0.5
+                            
+                console.log($progress);
+                
+                window.addEventListener('scroll', event => {
+                }
+            
             }
 
         }
@@ -59,6 +79,9 @@
         accordion : {
             heading: '.body-component > a',
             body: '.body-component',
+        },
+        progressbar : {
+            progress: '.scroll-indicator',
         }
     }
     
@@ -66,6 +89,7 @@
 
     try {
         article.accordion();
+        article.progressBar();
     } catch (e) {
         console.warn("You have some error(s):")
         console.log(e.name);
